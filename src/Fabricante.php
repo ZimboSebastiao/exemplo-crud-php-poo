1edea8598a6a1afa4780bffba34dc54a6ced8c60
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-10-05 09:41:47 -0300

    método atualizar fabricante
    refatorando com poo

--- src/Fabricante.php
+++ src/Fabricante.php
@@ -63,6 +63,19 @@ final class Fabricante {
     } 
 
 
+    public function atualizarFabricante():void {
+        $sql = "UPDATE fabricantes SET nome = :nome WHERE id = :id";
+        
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":nome", $this->nome, PDO::PARAM_STR);
+            $consulta->bindValue(":id", $this->id, PDO::PARAM_INT);
+            $consulta->execute();
+        } catch (Exception $erro) {
+            die("Erro ao atualizar: ".$erro->getMessage());
+        }
+    }
+
 
 
     public function getId(): int {
@@ -70,7 +83,7 @@ final class Fabricante {
     }
 
     public function setId(int $id): self {
-        $this->id = $id;
+        $this->id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
         return $this;
     }
 
